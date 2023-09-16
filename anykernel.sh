385e31d092395d9710f9345ec31d63a77df728b9
devilprobir <144839255+devilprobir@users.noreply.github.com>
2023-09-16 06:48:32 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Blaze+ Kernel by Probir @ telegram
+kernel.string=Blaze+ Kernel by Probir @officialpro - Telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
